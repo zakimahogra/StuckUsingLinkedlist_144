6be76decb6ce816c4c011c1b19c68ba97167ed69
M S I <M S I@MSI>
2024-05-10 10:23:56 +0700

    switch case 2

--- StuckUsingLinked/StuckUsingLinked.cpp
+++ StuckUsingLinked/StuckUsingLinked.cpp
@@ -76,6 +76,14 @@ int main() {
 			cin >> value;
 			stack.push(value);
 			break;
+		case 2:
+			if (!stack.isEmpty()) {
+				stack.pop();
+			}
+			else {
+				cout << "Stack is empty. Cannot pop." << endl;
+			}
+			break;
 		}
 	}
 }
