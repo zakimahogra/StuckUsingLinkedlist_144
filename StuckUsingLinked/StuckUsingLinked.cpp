2659cdcc5535d0cfc647a394393423a7ba5f6eb8
M S I <M S I@MSI>
2024-05-10 10:16:18 +0700

    else current

--- StuckUsingLinked/StuckUsingLinked.cpp
+++ StuckUsingLinked/StuckUsingLinked.cpp
@@ -41,5 +41,12 @@ public:
 		if (top == NULL) {
 			cout << "List is empty. " << endl;
 		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << "" << endl;
+				current = current->next;
+			}
+		}
 	}
 };
