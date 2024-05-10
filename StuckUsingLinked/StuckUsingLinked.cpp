1993f9e0f5cf82d2dea7a9dc27f97060d2d038b5
M S I <M S I@MSI>
2024-05-10 10:22:14 +0700

    switch case 1

--- StuckUsingLinked/StuckUsingLinked.cpp
+++ StuckUsingLinked/StuckUsingLinked.cpp
@@ -69,5 +69,13 @@ int main() {
 		cout << "4. Exit\n";
 		cout << "Enter your choice: ";
 		cin >> choice;
+
+		switch (choice) {
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value);
+			break;
+		}
 	}
 }
